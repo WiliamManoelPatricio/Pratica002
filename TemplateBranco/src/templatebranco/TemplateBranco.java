ddd0971e9654898844a854d7a612236a90a0dd50
WILIAM MANOEL PATRICIO <wiliam.patricio@alunos.sc.senac.br>
2021-08-03 21:42:17 -0300

    Atualização 04

---
+++ TemplateBranco/src/templatebranco/TemplateBranco.java
@@ -0,0 +1,157 @@
+/*
+Escreve em duas ou mais linhas
+*/
+package templatebranco;
+
+/**
+ *                                                                                      
+ * @author wiliam.patricio;
+ */
+public class TemplateBranco {
+
+    /**
+     * @param args the command line arguments;
+     */
+    public static void main(String[] args) {
+        String str = "Olá Mundo";
+        int z = 6; 
+        int x = 7;
+        int w = z + x;
+        int valor1 = 1;
+        int valor2 = 2;
+        
+        System.out.println(str); 
+        System.out.println("6 + 7 = "  + w); // PrintLn (L minusculo), nesse código é feita uma expressão de 6 + 7;
+        System.out.println(valor1 + valor2);
+        
+      // Pratica
+      
+       int a = 1;
+       int b = 2;
+
+         System.out.println("Sistema iniciando...");
+         System.out.println(a + b);
+         System.out.println("Sistema fechando...");
+         
+        // Outro dia no curso
+
+        int[] ListNum = {20, 10, 5, 25};
+        int resp = 30;
+        
+     System.out.println("Sistema Iniciando, Aula Pratica 003");
+        if(ListNum[0] + ListNum[0] == resp) {
+            System.out.println("Resposta Encontrada!");
+            System.out.printf("O resultado do cálculo",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[0] + ListNum[1] == resp) {
+            System.out.println("Resposta Encontrada!");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[0] + ListNum[2] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+            
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[0] + ListNum[3] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[1] + ListNum[0] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[1] + ListNum[1] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[1] + ListNum[2] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[1] + ListNum[3] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[2] + ListNum[0] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[2] + ListNum[1] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[2] + ListNum[2] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[2] + ListNum[3] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[3] + ListNum[0] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[3] + ListNum[1] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[3] + ListNum[2] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        if(ListNum[3] + ListNum[3] == resp) {
+            System.out.println("Resposta Encontrada");
+            System.out.printf("A resposta é %d ",resp);
+        }
+        else{ 
+            System.out.println("Resposta Errada");
+           }
+        }
+    }
+   
